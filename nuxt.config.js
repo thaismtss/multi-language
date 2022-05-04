31473434f24724a0cdb946b3552d6b6a68fdf579
Thais Martins <thaismartins1999@gmail.com>
2022-05-04 08:04:37 -0300

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'multi-language',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/index.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    ['@nuxtjs/i18n']
+  ],
+
+  i18n: {
+    locales: ['en', 'pt'],
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: {
+          message: 'Welcome'
+        },
+        pt: {
+          message: 'Bem Vindo'
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="container">
+  <h1 class="title">{{ $t('message') }}</h1>
+  <div class="container-btns">
+    <nuxt-link class="btn" :to="switchLocalePath('pt')">Português</nuxt-link>
+    <nuxt-link class="btn" :to="switchLocalePath('en')">English</nuxt-link>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+  .container {
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }  
+  .container-btns {
+    display: flex;
+  }
+  .btn {
+    text-decoration: none;
+    background: rgb(44, 162, 220);
+    color: white;
+    padding: 1rem;
+    font-size: 1.5rem;
+    width: 120px;
+    margin: 0.5rem;
+    text-align: center;
+  }
+  .nuxt-link-exact-active {
+    font-weight: bold;
+    background: rgb(36, 139, 191);
+    
+}
+</style>
